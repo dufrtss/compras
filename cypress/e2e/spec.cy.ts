61adb1b9ece88873a2e1b97dbb61a2b8cbe56fa6
Eduardo Freitas <eduardofragadefreitas@gmail.com>
2023-06-24 12:33:38 -0300

    add first testing script :tada:

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,19 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Teste Solicitação de compra', () => {
+  it('Adiciona um elemento na solicitação de compra', () => {
     cy.visit('http://localhost:5173/')
+
+    cy.get("#name").type("Cabo de cobre")
+
+    cy.get("#amount").type("10")
+    
+    cy.contains("Add").click()
+
+    cy.get("#productTable").should("contain", "Cabo de rede").find("#trash").click()
+
+    cy.get("#name").type("Carregador usb-c")
+
+    cy.get("#amount").type("3")
+    
+    cy.contains("Add").click()
   })
 })
