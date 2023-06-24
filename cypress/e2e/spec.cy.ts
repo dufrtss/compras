983be5d7d00f274ca5232144324351d57b9f428d
Eduardo Freitas <eduardofragadefreitas@gmail.com>
2023-06-24 14:01:47 -0300

    wip testing suite :penguin:

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,19 +1,37 @@
-describe('Teste Solicitação de compra', () => {
-  it('Adiciona um elemento na solicitação de compra', () => {
+describe('Adicionar produtos na solicitação de compra', () => {
+  it('Adiciona três produtos na solicitação de compra', () => {
     cy.visit('http://localhost:5173/')
 
     cy.get("#name").type("Cabo de cobre")
 
-    cy.get("#amount").type("10")
+    cy.get("#amount").type("{backspace}10")
     
     cy.contains("Add").click()
-
-    cy.get("#productTable").should("contain", "Cabo de rede").find("#trash").click()
 
     cy.get("#name").type("Carregador usb-c")
 
-    cy.get("#amount").type("3")
+    cy.get("#amount").type("{backspace}3")
     
     cy.contains("Add").click()
+
+    cy.get("#name").type("Notebooks")
+
+    cy.get("#amount").type("{backspace}25")
+    
+    cy.contains("Add").click()
+  })
+})
+
+describe('Remover um produto na solicitação de compra', () => {
+  it('Adiciona e remove um produto na solicitação de compra', () => {
+    cy.visit('http://localhost:5173/')
+
+    cy.get("#name").type("Caixa de som")
+
+    cy.get("#amount").type("{backspace}2")
+    
+    cy.contains("Add").click()
+
+    cy.get("#trash").click()
   })
 })
