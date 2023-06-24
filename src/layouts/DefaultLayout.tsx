85229cfffad0a812c8b4602cc3ca8d27372e8098
Eduardo Freitas <eduardofragadefreitas@gmail.com>
2023-06-24 16:17:38 -0300

    edit default layout style :apple:

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -4,7 +4,7 @@ import { Header } from "../components/Header";
 export function DefaultLayout() {
   return (
     <>
-      <div className="flex flex-col gap-10">
+      <div className="flex flex-col gap-5">
         <Header />
         <Outlet />
       </div>
