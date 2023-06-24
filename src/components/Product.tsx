6d16c90790e247e0992547355f5ec794999caa71
Eduardo Freitas <eduardofragadefreitas@gmail.com>
2023-06-24 16:19:51 -0300

    changed styles on Product component :panda:

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -14,18 +14,16 @@ export function Product({ id, name, amount, onDeleteProduct }: ProductProps) {
 
   return (
     <>
-      <div className="grid grid-flow-col grid-cols-4 py-4 px-5 w-1/2 bg-zinc-700 outline-none rounded-md border-2 border-zinc-600">
-        <span>{id}</span>
-        <span className="col-span-2">{name}</span>
-        <span className="ml-4">{amount}</span>
-        <button
-          id="trash"
-          onClick={handleDeleteProduct}
-          className="outline-none mr-3 rounded-sm focus:outline-cyan-600 hover:text-cyan-600 transition-colors"
-        >
-          <Trash size={16} />
-        </button>
-      </div>
+      <span>{id}</span>
+      <span className="col-span-2">{name}</span>
+      <span className="ml-4">{amount}</span>
+      <button
+        id="trash"
+        onClick={handleDeleteProduct}
+        className="outline-none mr-3 rounded-sm focus:outline-cyan-600 hover:text-cyan-600 transition-colors"
+      >
+        <Trash size={16} />
+      </button>
     </>
   );
 }
