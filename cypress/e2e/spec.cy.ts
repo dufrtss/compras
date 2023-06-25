fcfd1619641fdde27ae032201f7d0645cd6d1186
Eduardo Freitas <eduardofragadefreitas@gmail.com>
2023-06-25 13:19:07 -0300

    add final test :tada:

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -71,3 +71,23 @@ describe('Adicionar e remover itens antes do pedido de compra', () => {
     cy.contains("Enviar pedido").click()
   })
 })
+
+describe('Gera nota fiscal no fim de um pedido de compra', () => {
+  it('Acessa, registra, envia um pedido de compra e gera a nota fiscal', () => {
+    cy.visit('http://localhost:5173/')
+
+    cy.contains("Pedido de compra").click()
+
+    cy.get("#supplier").type("Useall")
+
+    cy.get("#shipping").type("50")
+
+    cy.get("#payMethod").type("À Vista")
+
+    cy.get("#exitType").type("Matéria Prima")
+
+    cy.contains("Enviar pedido").click()
+
+    cy.contains("Gerar nota fiscal").click()
+  })
+})
